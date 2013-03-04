37467d85a24506f59e76b5dc134406b9815c6a10
VuoriLiikaluoma <vuoriliikaluoma@me.com>
2013-03-04 03:58:04 -0500

    Now with X..Z style character range support.

--- mb_trim.php
+++ mb_trim.php
@@ -37,8 +37,19 @@ if (extension_loaded('mbstring')) {
 					$charlist = "\\x{20}\\x{9}\\x{A}\\x{D}\\x{0}\\x{B}"; // Standard charlist, same as trim.
 				} else {
 					$chars = preg_split('//u', $charlist, -1, PREG_SPLIT_NO_EMPTY); // Splits the string into an array, character by character.
-					foreach ($chars as &$char) {
-						$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+					foreach ($chars as $c => &$char) {
+						if (preg_match('/^\x{2E}$/u', $char) && preg_match('/^\x{2E}$/u', $chars[$c+1])) {
+							$ch1 = hexdec(substr($chars[$c-1], 3, -1));
+							$ch2 = (int)substr(mb_encode_numericentity($chars[$c+2], [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1);
+							$chs = '';
+							for ($i = $ch1; $i <= $ch2; $i++) {
+								$chs .= "\\x{" . dechex($i) . "}";
+							}
+							unset($chars[$c], $chars[$c+1], $chars[$c+2]); // Unset the now pointless values.
+							$chars[$c-1] = $chs; // Set the range.
+						} else {
+							$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+						}
 					}
 					$charlist = implode('', $chars); // Return the array to string type.
 				}
@@ -69,8 +80,19 @@ if (extension_loaded('mbstring')) {
 					$charlist = "\\x{20}\\x{9}\\x{A}\\x{D}\\x{0}\\x{B}"; // Standard charlist, same as trim.
 				} else {
 					$chars = preg_split('//u', $charlist, -1, PREG_SPLIT_NO_EMPTY); // Splits the string into an array, character by character.
-					foreach ($chars as &$char) {
-						$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+					foreach ($chars as $c => &$char) {
+						if (preg_match('/^\x{2E}$/u', $char) && preg_match('/^\x{2E}$/u', $chars[$c+1])) {
+							$ch1 = hexdec(substr($chars[$c-1], 3, -1));
+							$ch2 = (int)substr(mb_encode_numericentity($chars[$c+2], [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1);
+							$chs = '';
+							for ($i = $ch1; $i <= $ch2; $i++) {
+								$chs .= "\\x{" . dechex($i) . "}";
+							}
+							unset($chars[$c], $chars[$c+1], $chars[$c+2]); // Unset the now pointless values.
+							$chars[$c-1] = $chs; // Set the range.
+						} else {
+							$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+						}
 					}
 					$charlist = implode('', $chars); // Return the array to string type.
 				}
@@ -101,10 +123,20 @@ if (extension_loaded('mbstring')) {
 					$charlist = "\\x{20}\\x{9}\\x{A}\\x{D}\\x{0}\\x{B}"; // Standard charlist, same as trim.
 				} else {
 					$chars = preg_split('//u', $charlist, -1, PREG_SPLIT_NO_EMPTY); // Splits the string into an array, character by character.
-					foreach ($chars as &$char) {
-						$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+					foreach ($chars as $c => &$char) {
+						if (preg_match('/^\x{2E}$/u', $char) && preg_match('/^\x{2E}$/u', $chars[$c+1])) {
+							$ch1 = hexdec(substr($chars[$c-1], 3, -1));
+							$ch2 = (int)substr(mb_encode_numericentity($chars[$c+2], [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1);
+							$chs = '';
+							for ($i = $ch1; $i <= $ch2; $i++) {
+								$chs .= "\\x{" . dechex($i) . "}";
+							}
+							unset($chars[$c], $chars[$c+1], $chars[$c+2]); // Unset the now pointless values.
+							$chars[$c-1] = $chs; // Set the range.
+						} else {
+							$char = "\\x{" . dechex(substr(mb_encode_numericentity($char, [0x0, 0x10ffff, 0, 0x10ffff], $encoding), 2, -1)) . "}"; // Convert the character to it's unicode codepoint in \x{##} format.
+						}
 					}
-					var_dump($chars);
 					$charlist = implode('', $chars); // Return the array to string type.
 				}
 				$pattern = '/([' . $charlist . ']+$)/u'; // Define the pattern.
